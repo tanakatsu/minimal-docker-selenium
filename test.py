0114f66c49f364478aebaf26aece675e1914305a
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2022-05-17 22:35:49 +0900

    Add Dockefile for installing chromium

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options, executable_path='/opt/chrome/chromedriver')
+# driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')  # when you install chromium and chromium-driver
 
 driver.get('https://qiita.com')
 print(driver.current_url)
